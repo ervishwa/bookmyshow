ebdfc19853d214b461675a0c59c351bb656d2e93
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-02-16 17:07:12 +0530

    intial

---
+++ notes.md
@@ -0,0 +1,4 @@
+api key - acbd93bea5123c355b3169cdda353d0d
+calling api - https://api.themoviedb.org/3/movie/550?api_key=acbd93bea5123c355b3169cdda353d0d
+
+generes - https://api.themoviedb.org/3/genre/movie/list?api_key=acbd93bea5123c355b3169cdda353d0d&language=en-US
--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { Header } from './Components/Header/Header';
+import { Home } from './Page/Home/Home';
+import {Moviedetail} from "./Page/MovieDetail/MovieDetail"
+import { MovieList } from './Components/MovieList/MovieList';
+import Login from './Components/Signin/Login';
+import Signup from './Components/Signup/Signup';
+import { TicketBooking } from './Components/TicketBooking/TicketBooking';
+import { CheckOut } from './Components/CheckOut/CheckOut';
+import { useFirebase } from './Firebase';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Header></Header>
+         <Routes>
+            <Route index element={<Home></Home>}></Route>
+            <Route path='movie/:id' element={<Moviedetail></Moviedetail>}></Route> 
+            <Route path='movies/:type' element={<MovieList></MovieList>}></Route>
+            <Route path='movies/:type/movie/:id' element={<Moviedetail></Moviedetail>}></Route>
+            <Route path='/ticketbook' element={<TicketBooking/>}></Route>
+
+            <Route path="/checkout" element={<CheckOut/>} /> 
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/signup' element={<Signup/>}></Route>
+            <Route path='/*' element={<h2>Error</h2>}></Route> 
+             
+         </Routes>
+       </Router>
+      </div>
+
+    //<TicketBooking></TicketBooking>
   );
 }
 
